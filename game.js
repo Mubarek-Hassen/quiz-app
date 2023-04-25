606bb1d56388ca35810da3345c55eab7664f9a6a
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2023-04-24 21:34:19 -0400

    working on visuals for right/wrong answers

--- game.js
+++ game.js
@@ -1,10 +1,9 @@
 const question = document.getElementById("question")
 
 const choices = Array.from(document.getElementsByClassName("choice-text"))
-console.log(choices)
 
 let currentQuestion = {};
-let acceptingAnswers = true;
+let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
@@ -40,6 +39,9 @@ startGame =()=>{
 }
 
 getNewQuestion=()=>{
+  if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS){
+    return window.location.assign("/end.html")
+  }
   questionCounter++;
   const questionIndex = Math.floor(Math.random() * availableQuestions.length);
   currentQuestion = availableQuestions[questionIndex];
@@ -48,6 +50,19 @@ getNewQuestion=()=>{
     const number = choice.dataset["number"];
     choice.innerText = currentQuestion['choice' + number];
   });
+  availableQuestions.splice(questionIndex, 1);
+  acceptingAnswers = true;
 }
+choices.forEach(choice =>{
+  choice.addEventListener("click", e =>{
+    if(!acceptingAnswers) return;
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset['number'];
+    const classToApply = selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+    
+    getNewQuestion();
+  })
+})
 
 startGame();
