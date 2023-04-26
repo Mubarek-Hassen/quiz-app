ff725511662173e63812fb63a29be6d33daa5bbd
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2023-04-26 16:52:18 -0400

    working on HUD

--- game.js
+++ game.js
@@ -64,9 +64,7 @@ choices.forEach(choice =>{
     setTimeout(()=>{
       selectedChoice.parentElement.classList.remove(classToApply)
       getNewQuestion();
-    }, 1000)
-    // selectedChoice.parentElement.classList.remove(classToApply)
-    // getNewQuestion();
+    }, 1000);
   })
 })
 
