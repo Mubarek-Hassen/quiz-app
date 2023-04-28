692ad483f16945d6c83d7ab9441320b8b3acd0d0
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2023-04-27 23:00:20 -0400

    added progress bar

--- game.js
+++ game.js
@@ -1,10 +1,8 @@
 const question = document.getElementById("question")
-
 const choices = Array.from(document.getElementsByClassName("choice-text"))
-
-const questionCounterText = document.getElementById("questionCounter")
-
+const progressText = document.getElementById("progressText")
 const scoreText = document.getElementById("score")
+const progressBarFull = document.getElementById("progressBarFull")
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -32,7 +30,6 @@ let questions = [
 ];
 
 const CORRECT_BONUS = 10;
-
 const MAX_QUESTIONS = 2;
 
 startGame =()=>{
@@ -47,7 +44,8 @@ getNewQuestion=()=>{
     return window.location.assign("/end.html")
   }
   questionCounter++;
-  questionCounterText.innerText = `${questionCounter}/ ${MAX_QUESTIONS}`
+  progressText.innerText = `Question ${questionCounter}/ ${MAX_QUESTIONS}`
+  progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS)*100}%`
   const questionIndex = Math.floor(Math.random() * availableQuestions.length);
   currentQuestion = availableQuestions[questionIndex];
   question.innerText = currentQuestion.question;
@@ -58,6 +56,7 @@ getNewQuestion=()=>{
   availableQuestions.splice(questionIndex, 1);
   acceptingAnswers = true;
 }
+
 choices.forEach(choice =>{
   choice.addEventListener("click", e =>{
     if(!acceptingAnswers) return;
