c5e766553eb71e30e40b094479a826bb72ae4a02
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2023-04-30 22:00:39 -0400

    explanation still misunderstood

--- game.js
+++ game.js
@@ -24,6 +24,9 @@ fetch("https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=mul
     const answerChoices = [...loadedQuestion.incorrect_answers];
     formattedQuestion.answer = Math.floor(Math.random() * 3) + 1;
     answerChoices.splice(formattedQuestion.answer -1, 0, loadedQuestion.correct_answer)
+    answerChoices.forEach((choice, index)=>{
+      formattedQuestion["choice" + (index + 1)] = choice
+    })
   } )
   // questions = loadedQuestion;
   // startGame()
