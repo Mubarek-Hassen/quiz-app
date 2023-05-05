18e6e9fc8cf465bf96aec40cad47b559918e3e67
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2023-05-04 21:20:28 -0400

    quiz app completed

--- game.js
+++ game.js
@@ -3,7 +3,8 @@ const choices = Array.from(document.getElementsByClassName("choice-text"))
 const progressText = document.getElementById("progressText")
 const scoreText = document.getElementById("score")
 const progressBarFull = document.getElementById("progressBarFull")
-const loader = getElementById("loader")
+const loader = document.getElementById("loader")
+const game = document.getElementById("game")
 let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
@@ -36,13 +37,15 @@ fetch("https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=mul
   })
 
 const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 2;
+const MAX_QUESTIONS = 10;
 
 startGame =()=>{
   questionCounter = 0;
   score = 0;
   availableQuestions = [...questions]
   getNewQuestion()
+  game.classList.remove("hidden")
+  loader.classList.add("hidden")
 }
 
 getNewQuestion=()=>{
